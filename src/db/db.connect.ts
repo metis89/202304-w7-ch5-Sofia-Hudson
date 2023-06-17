a54a158ac6464afcb3fd1d6a684815c48d2fa689
Sofia Hudson <sofia-sama@hotmail.com>
2023-06-17 21:48:08 +0200

    Fix uri error in db.connect

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { db, passwd, user } from '../config.js';
 
 export const dbConnect = () => {
-  const uri = `mongodb+srv://${user}:${passwd}@cluster0.pzctaqf.mongodb.net/${db}?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${user}:${passwd}@cluster0.hbqvi6r.mongodb.net/${db}?retryWrites=true&w=majority`;
+
   return mongoose.connect(uri);
 };
